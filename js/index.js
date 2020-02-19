a032c7bbead16d25063079af160d44994bdf2f00
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2020-02-18 16:11:10 -0800

    7 events redone, still working to finish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+const navTop = document.querySelector('.nav');
+navTop.addEventListener('mouseover', () => {
+    event.target.style.color = 'red';
+});
+navTop.addEventListener('mouseout', () => {
+    event.target.style.color = 'blueviolet';
+});
+
+const fImage = document.querySelector('img');
+fImage.addEventListener('mousedown', () => {
+    event.target.style.transform = 'scale(1.5)';
+});
+fImage.addEventListener('mouseup', () => {
+    event.target.style.transform = 'scale(1)';
+});
+
+window.addEventListener('resize', () => {
+    alert('Not THAT size!')
+});
+
+const fPage = document.querySelector('body');
+fPage.addEventListener('dblclick', () => {
+    fPage.style.backgroundColor = '#2E9951';
+});
+
+const dText = document.querySelector('.content-pick');
+dText.addEventListener('copy', () => {
+    dText.style.backgroundColor = '#008B8B';
+})
+
+
