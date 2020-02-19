9ab14006846bebaeb5377a7c051c5de2734c10fa
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2020-02-18 17:02:05 -0800

    finished mvp

--- js/index.js
+++ js/index.js
@@ -6,13 +6,17 @@ navTop.addEventListener('mouseout', () => {
     event.target.style.color = 'blueviolet';
 });
 
-const fImage = document.querySelector('img');
-fImage.addEventListener('mousedown', () => {
-    event.target.style.transform = 'scale(1.5)';
+const fImage = document.querySelector('.intro img');
+fImage.addEventListener('pointerover', () => {
+    fImage.style.transition = '2s ease-in'
+    fImage.style.transform = 'scale(1.3)';
 });
-fImage.addEventListener('mouseup', () => {
-    event.target.style.transform = 'scale(1)';
+fImage.addEventListener('pointerout', () => {
+    fImage.style.transform = 'scale(1)';
 });
+fImage.addEventListener('transitionrun', () => {
+    event.target.style.border = '3px #4B0082 solid';
+})
 
 window.addEventListener('resize', () => {
     alert('Not THAT size!')
@@ -26,6 +30,14 @@ fPage.addEventListener('dblclick', () => {
 const dText = document.querySelector('.content-pick');
 dText.addEventListener('copy', () => {
     dText.style.backgroundColor = '#008B8B';
-})
+});
 
+const dImage = document.querySelector('.content-destination img');
+dImage.addEventListener('dragstart', () => {
+    dImage.style.transition = '.5s ease-in';
+    dImage.style.transform = 'scaleY(-1)';
+});
+dImage.addEventListener('dragend', () => {
+    dImage.style.transform = 'scaleY(1)';
+})
 
